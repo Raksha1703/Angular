513c11a60f77865d8e88197e0c4cae4561b311b2
Raksha1703 <rakshabarai173@gmail.com>
2021-02-24 18:06:04 +0530

    start

--- src/app/usertable/usertable.component.ts
+++ src/app/usertable/usertable.component.ts
@@ -1,18 +1,44 @@
 import { Component, OnInit } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
+ 
 @Component({
   selector: 'app-usertable',
   templateUrl: './usertable.component.html',
   styleUrls: ['./usertable.component.scss']
 })
 export class UsertableComponent implements OnInit {
-
-  constructor() { }
+  employees:any = [];
+  search = "";
+  loading = true;
+  pagination = "";
+  page = 1;
+  constructor(private http : HttpClient) { }
 
   ngOnInit(): void {
-    this.getallusers();
+    this.namechange();
   }
-  getallusers(){
-    console.log('all user loaded');
+  namechange(obj = ""){
+      this.http.get<any>('http://127.0.0.1:8000/api/employees?'+obj).subscribe(resp=> {
+      this.employees=resp.employee.data;
+      this.pagination=resp.employee.link;
+      this.loading = false;
+      console.log(this.employees);
+    },error=>{
+      console.log(error);
+      this.loading = true;
+    })
+  };
+  next(){
+    this.page = this.page + 1;
+    this.namechange();
+  };
+  pre(){
+    this.page = this.page - 1;
+    this.namechange();
+  };
+  getemployee(){
+    let obj =`name=${this.search}`;
+    this.namechange(obj);
+    this.namechange();
   };
 }

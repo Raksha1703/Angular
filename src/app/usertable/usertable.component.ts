3fb31bd6661446fc81b4525b900fc8e6e2d5fddd
Raksha1703 <rakshabarai173@gmail.com>
2021-02-25 17:22:35 +0530

    complete

--- src/app/usertable/usertable.component.ts
+++ src/app/usertable/usertable.component.ts
@@ -1,80 +1,110 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { FormControl } from '@angular/forms';
+
 @Component({
   selector: 'app-usertable',
   templateUrl: './usertable.component.html',
-  styleUrls: ['./usertable.component.scss']
+  styleUrls: ['./usertable.component.scss'],
 })
 export class UsertableComponent implements OnInit {
-  employees:any = [];
-  search = "";
-  name ="";
-  position = "";
-  office  = "";
+  employees: any = [];
+  
+  search = '';
+  id = 0;
+  name = '';
+  position = '';
+  office = '';
   age;
   startdate;
   salary;
   loading = true;
-  pagination = "";
+  pagination = '';
   page = 1;
-  constructor(private http : HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   ngOnInit(): void {
     this.namechange();
   }
-  namechange(obj = ""){
-      this.http.get<any>('http://127.0.0.1:8000/api/employees?'+obj).subscribe(resp=> {
-      this.employees=resp.employee.data;
-      this.pagination=resp.employee.link;
-      this.loading = false;
-      console.log(this.employees);
-    },error=>{
-      console.log(error);
-      this.loading = true;
-    })
-  };
-  next(){
+  namechange(obj = '') {
+    this.http.get<any>('http://127.0.0.1:8000/api/employees?' + obj).subscribe(
+      (resp) => {
+        this.employees = resp.employee.data;
+        this.pagination = resp.employee.link;
+        this.loading = false;
+        console.log(this.employees);
+      },
+      (error) => {
+        console.log(error);
+        this.loading = true;
+      }
+    );
+  }
+  next() {
     this.page = this.page + 1;
     this.namechange();
-  };
-  pre(){
+  }
+  pre() {
     this.page = this.page - 1;
     this.namechange();
-  };
-  getemployee(){
-    let obj =`name=${this.search}`;
+  }
+  getemployee() {
+    let obj = `name=${this.search}`;
     this.namechange(obj);
     this.namechange();
-  };
-  addemployee(){
-    let data ={      
-      name:this.name,
-      position:this.position,
-      office:this.office,
-      age:this.age,
-      startdate:this.startdate,
-      salary:this.salary   
+  }
+  addemployee() {
+    let data = {
+      name: this.name,
+      position: this.position,
+      office: this.office,
+      age: this.age,
+      startdate: this.startdate,
+      salary: this.salary,
     };
     console.log(data);
-    this.http.post<any>("http://127.0.0.1:8000/api/employees",data).subscribe(resp=> {   
-     console.log(resp);
-     this.name = this.position = this.age = this.office =this.startdate = this.salary = "";
-     this.getemployee();
-    },error=>{
-      console.log(error);     
-    })
-  };
-  updateemployee(emp){
-    console.log();
-  };
-  deleteemployee(id){
-    let url="http://127.0.0.1:8000/api/employees/";
-    this.http.delete(url+id)
-    .subscribe(resp => {
+    this.http.post<any>('http://127.0.0.1:8000/api/employees', data).subscribe(
+      (resp) => {
+        console.log(resp);
+        this.name = this.position = this.age = this.office = this.startdate = this.salary ='';
+        this.getemployee();
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
+  updateemployee(emp) {
+    this.id = emp.id;
+    this.name = emp.name;
+    this.position = emp.position;
+    this.age = emp.age;
+    this.office = emp.office;
+    this.startdate = new Date(emp.startdate);
+    this.salary = emp.salary;
+  }
+  editemployee() {
+    let data = {
+      name: this.name,
+      position: this.position,
+      office: this.office,
+      age: this.age,
+      startdate: this.startdate,
+      salary: this.salary,
+    };
+    this.http
+      .put<any>('http://127.0.0.1:8000/api/employees/'+this.id,data)
+      .subscribe((resp) => {
+        console.log(resp);
+        this.name = this.position = this.age = this.office = this.startdate = this.salary =
+          '';
+        this.getemployee();
+      });
+  }
+  deleteemployee(id) {
+    let url = 'http://127.0.0.1:8000/api/employees/';
+    this.http.delete(url + id).subscribe((resp) => {
       console.log(resp);
-     this.getemployee();
+      this.getemployee();
     });
-  };
-
+  }
 }
